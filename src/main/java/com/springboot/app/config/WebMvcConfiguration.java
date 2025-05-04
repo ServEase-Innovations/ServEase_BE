31b3a1f42043e66badf09321994a7f986a15f515
Ronit <maity.ronit18@gmail.com>
2025-05-04 22:12:32 +0530

    fix origins

--- src/main/java/com/springboot/app/config/WebMvcConfiguration.java
+++ src/main/java/com/springboot/app/config/WebMvcConfiguration.java
@@ -20,7 +20,7 @@ public class WebMvcConfiguration
             public void addCorsMappings(CorsRegistry registry) {
                 // allow all origins to access our service
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:3000", "https://abcdefg.ngrok.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
                         .allowedHeaders("*");
             }
